5b2ee4a1bda2085ecd07c012808c377a7a27c8ad
kate-souto <2024323468@aluno.riogrande.ifrs.edu.br>
2026-04-05 01:52:00 -0300

    Add files via upload

---
+++ trabalho/removerprof.php
@@ -0,0 +1,31 @@
+<?php
+    require_once 'db.php';
+
+    $cpf = $_POST["id_para_prof_ser_removido"];
+
+    $conexao = getConexao();
+
+    if (!$conexao) 
+    {
+        die("Falha na conexão: " . mysqli_connect_error());
+    }
+
+
+    mysqli_query($conexao, "DELETE FROM materias WHERE cpfprofessor = '$cpf'");
+    $query = "DELETE FROM professor WHERE cpf = '$cpf'";
+    //echo "Query SQL: " . $query . "<br>";
+
+    if (mysqli_query($conexao, $query)) 
+    {
+        echo "Professor removido com sucesso!";
+    } 
+    else 
+    {
+        echo "Erro ao remover professor: " . mysqli_error($conexao);
+    }
+
+    mysqli_close($conexao);
+
+    header('Location: home.php')
+
+?>
