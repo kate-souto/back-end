985c9ec433c345c715fc89bfdb11b1a10d9caafc
kate-souto <2024323468@aluno.riogrande.ifrs.edu.br>
2026-04-04 22:26:46 -0300

    Add files via upload

---
+++ trabaio/remover.php
@@ -0,0 +1,25 @@
+<?php
+    require_once 'db.php';
+
+    $codigo = $_POST["id_para_ser_removido"];
+
+    $conexao = getConexao();
+
+    if (!$conexao) {
+        die("Falha na conexão: " . mysqli_connect_error());
+    }
+
+    $query = "DELETE FROM materias WHERE codigo = $codigo";
+    //echo "Query SQL: " . $query . "<br>";
+
+    if (mysqli_query($conexao, $query)) {
+        echo "Matéria removida com sucesso!";
+    } else {
+        echo "Erro ao remover matéria: " . mysqli_error($conexao);
+    }
+
+    mysqli_close($conexao);
+
+    header('Location: home.php')
+
+?>
