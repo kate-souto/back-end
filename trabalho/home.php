2d25c7ef854610be1a55261a73633eee235735a7
kate-souto <2024323468@aluno.riogrande.ifrs.edu.br>
2026-04-07 16:03:49 -0300

    Add files via upload

---
+++ trabalho/home.php
@@ -0,0 +1,183 @@
+<?php
+require_once 'db.php';
+$conexao = getConexao();
+
+if (!$conexao) 
+{
+    die("Falha na conexão: " . mysqli_connect_error());
+}
+
+
+
+$botaomat = $_GET['botaomat'] ?? '';
+
+$query = "SELECT materias.nomemateria, materias.codigo, professor.nome AS nomeprofessor FROM materias LEFT JOIN professor ON materias.cpfprofessor = professor.cpf";
+
+if ($botaomat == "ordenar")
+{
+    $query .= " ORDER BY materias.nomemateria ASC";
+}
+
+$resultado = mysqli_query($conexao, $query);
+
+?>
+
+
+
+<br>
+<a href="inserir.php">Inserir nova materia</a>
+<br>
+
+<table border="1">
+    <tr>
+        <th>Nome</th>
+        <th>Código</th>
+        <th>Professor</th>
+        <th> Remover </th>
+        <th> Editar </th>
+    </tr>
+
+
+
+<?php
+
+
+while ($row = mysqli_fetch_assoc($resultado)) 
+{
+    echo     
+    "<tr>
+        <td>" . $row["nomemateria"] . "</td>
+        <td>" . $row["codigo"] . "</td>
+        <td>" . $row["nomeprofessor"] . "</td>"  . 
+        "<td> 
+           
+        
+            <form action='remover.php' method='POST'>
+                <input type='hidden' name='id_para_ser_removido' value='" . $row["codigo"] . "'>
+                <input type='submit' value='Remover'>
+            </form>
+            
+        
+        </td>" .
+        
+        
+        "<td> 
+        
+
+            <form action='form_edit.php' method='POST'>
+                <input type='hidden' name='id_para_ser_editado' value='" . $row["codigo"] . "'>
+                <input type='submit' value='Editar'>
+            </form>
+
+
+        </td>" .
+        
+    "</tr>";
+}
+echo "<br>";
+?>
+
+</table>
+
+<br>
+
+<?php
+echo "Total de materias: " . mysqli_num_rows($resultado);
+
+
+?>
+
+<br><br>
+
+<a href="home.php?botaomat=ordenar">Ordenar em ordem alfabética</a>
+
+<br><br>
+<hr>
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+$botaoprof = $_GET['botaoprof'] ?? '';
+
+$query = "SELECT nome, cpf FROM professor";
+
+if ($botaoprof == "ordenar")
+{
+    $query .= " ORDER BY professor.nome ASC";
+}
+
+$resultado = mysqli_query($conexao, $query);
+
+?>
+
+<br>
+<a href="inserirprof.php">Inserir novo professor</a>
+<br>
+
+<table border="1">
+    <tr>
+        <th>Nome</th>
+        <th>CPF</th>
+        <th> Remover </th>
+        <th> Editar </th>
+    </tr>
+
+
+
+<?php
+
+
+while ($row = mysqli_fetch_assoc($resultado)) {
+    echo     
+    "<tr>
+        <td>" . $row["nome"] . "</td>
+        <td>" . $row["cpf"] . "</td>"  . 
+        "<td> 
+           
+        
+            <form action='removerprof.php' method='POST'>
+                <input type='hidden' name='id_para_prof_ser_removido' value='" . $row["cpf"] . "'>
+                <input type='submit' value='Remover'>
+            </form>
+            
+        
+        </td>" .
+        
+        
+        "<td> 
+        
+
+            <form action='form_editprof.php' method='POST'>
+                <input type='hidden' name='id_para_prof_ser_editado' value='" . $row["cpf"] . "'>
+                <input type='submit' value='Editar'>
+            </form>
+
+
+        </td>" .
+        
+    "</tr>";
+}
+echo "<br>";
+?>
+
+</table>
+
+<br>
+
+<?php
+echo "Total de professores: " . mysqli_num_rows($resultado);
+
+?>
+
+<br><br>
+<a href="home.php?botaoprof=ordenar">Ordenar em ordem alfabética</a>
